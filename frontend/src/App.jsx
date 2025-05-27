5ae565ed8b7cf85eeabb94672b8a67c421416b4b
Mission Kumar <missionk935@gmail.com>
2025-05-27 15:35:22 +0530

    Add frontend folder with Tailwind, pages, and stores

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white text-3xl p-6 rounded-md">
+        ✅ Tailwind is working!
+      </div>
+    </>
+  );
+}
+
+export default App;
